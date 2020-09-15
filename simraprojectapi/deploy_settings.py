767b47d3e54296d1796ae8efc246fbff0cc8a5e5
Fahim Al Islam <fahim6668@gmail.com>
2020-09-15 23:22:45 +0600

    Upadted: removed sslmode in DATABASE django

--- simraprojectapi/deploy_settings.py
+++ simraprojectapi/deploy_settings.py
@@ -14,7 +14,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
