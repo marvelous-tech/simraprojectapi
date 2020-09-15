85e8c83072182fb182f5ed297a8d87c19b6ba77e
Fahim Al Islam <fahim6668@gmail.com>
2020-09-15 22:29:23 +0600

    Initialized: initial commits!!!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn simraprojectapi.deploy_wsgi
---
+++ simraprojectapi/deploy_settings.py
@@ -0,0 +1,25 @@
+import dj_database_url
+from .settings import *
+
+SECURE = True if os.environ.get('SECURE', 'true') == 'true' else False
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if SECURE:
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
