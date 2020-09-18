bdad0fedf8fa7e1adfcd230e0ecc2e5f03610b46
tawhidularefindcc <tawhidularefin530@gmail.com>
2020-09-18 22:26:35 +0600

    Added: Field appointment type

--- appointment/models.py
+++ appointment/models.py
@@ -4,6 +4,12 @@ from django.utils import timezone
 
 from utils import random_string
 
+TYPE_CHOICES = (
+    ('U', 'URGENT_APPOINTMENT'),
+    ('R', 'ROUTINE_APPOINTMENT'),
+    ('H', 'HOME_VISIT_APPOINTMENT')
+)
+
 
 class Appointment(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='appointments')
@@ -14,6 +20,7 @@ class Appointment(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     token = models.CharField(auto_created=True, default=random_string, max_length=10, editable=False, unique=True)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
 
     def __str__(self):
         return self.token
